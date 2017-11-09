f0acfe1a2cdc2f28234d75cd6f95775a3759e815
FranciscoMera <fcomera.ipn@gmail.com>
2017-11-09 08:04:29 -0600

    Priner commit

---
+++ js/carr.js
@@ -0,0 +1,7 @@
+/**
+ * Created by franciscomera on 11/5/17.
+ */
+$(document).ready(function(){
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+});
